86ac66a3a692045ead9931acfbaeba204f17380a
15787 <15787000000a@gmail.com>
2024-03-08 09:06:34 +0300

    the fist commit to check if the repository is responsive

--- app/src/main/java/com/example/myshop/MainActivity.java
+++ app/src/main/java/com/example/myshop/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Views declaration
+    Button btnLogin;
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Views initialization
+        btnLogin = findViewById(R.id.btnSignIn);
+        signup = findViewById(R.id.signup);
+
+        //Set click listener on login button
+        btnLogin.setOnClickListener(v -> {
+            //Open login activity
+            startActivity(new Intent(MainActivity.this, Login.class));
+        });
+
     }
 }
